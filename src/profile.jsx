df945267d474c771fda32a84ed96b08253ef9d9f
zubairmh <zubair.mh@protonmail.com>
2023-04-29 01:31:33 +0530

    Added tailwindcss

---
+++ src/profile.jsx
@@ -0,0 +1,17 @@
+function Profile() {
+    return (
+        <div className="bg-black flex flex-col justify-center min-h-screen min-w-full">
+            <div className="bg-white w-32 h-32 rounded-full">
+
+            </div>
+            <div className="bg-white w-32 h-32 rounded-full">
+                
+            </div>
+            <div className="bg-white w-32 h-32 rounded-full">
+                
+            </div>    
+        </div>
+    );
+}
+
+export default Profile;
